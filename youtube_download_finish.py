26652e9384dd94b44c69b396618cf9182795d541
Shon <q79236@gmail.com>
2019-05-27 18:37:44 +0800

    move function to methods

--- youtube_download_finish.py
+++ youtube_download_finish.py
@@ -2,81 +2,12 @@ from selenium import webdriver
 # from selenium.webdriver.common.action_chains import ActionChains
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support.ui import WebDriverWait
-from utube_download import get_vedio
+import my_methods as my
 import os
-import time
 from bs4 import BeautifulSoup
 import multiprocessing.pool as p
 import threading
-
-
-
-
-def scroll(driver ,scroll_time):
-    count = 0
-    while True:
-        time.sleep(1)
-        driver.execute_script("window.scrollTo(0, " + str(32000 * count) + ")")
-        count = count +1
-        scroll_time = scroll_time -1
-        if not scroll_time :
-            break
-
-
-# #取消使用這個方法，太慢了
-# def open_windows_and_get_url(driver ,download_content):
-#     #其他人的Code
-#     # action = ActionChains(driver)
-#     # action.context_click(download_content).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
-#     #我的code
-#     # time.sleep(2) #沒有用
-#     action = ActionChains(driver)
-#     action.move_to_element(download_content)
-#     action.context_click()
-#     # action.click()  #變成在原本視窗跳轉
-#     action.pause(1)
-#     action.send_keys(Keys.ARROW_DOWN)
-#     action.send_keys(Keys.CONTROL ,'k')
-#     action.send_keys(Keys.ENTER)
-#     action.perform()
-#     time.sleep(random.randrange(1,5))
-    
-#     driver.switch_to_window(driver.window_handles[1])
-#     global download_url
-#     download_url = driver.current_url
-#     driver.close()
-#     time.sleep(1)
-#     driver.switch_to_window(driver.window_handles[0])
-#     # print('現在的url' , driver.current_url)
-#     # driver.forward() 記得在main中加入此動作
-
-#download_lists格式為[1,2,3]
-def process_create_thread_to_downlad(download_list):
-    threads = []
-    for download in download_list:
-        thread = threading.Thread(target = get_vedio ,args=(download,))
-        thread.start()
-        threads.append(thread)
-        time.sleep(1)
-    for thread in threads:
-        thread.join()
-
-
-def split_list(download_urls ,i):
-    #每i個url分一組，並儲存在download_lists中，格式為[[1,2,3],[4,5,6]。。]
-    download_lists = []
-    while len(download_urls):
-        temp = []
-        sp = i
-        while True:
-            sp = sp -1
-            temp.append(download_urls.pop(0))
-            if sp == 0 or len(download_urls) == 0:
-                break
-        download_lists.append(temp)
-    return download_lists
-    # print(download_lists)
-
+import time
 
 
 if __name__ =='__main__':
@@ -87,7 +18,7 @@ if __name__ =='__main__':
     driver = webdriver.Chrome(executable_path = driver_path)
     driver.get(url)
     time.sleep(1)
-    scroll(driver ,7)
+    my.scroll(driver ,7)
     #這裡得到的是經過ajax加載的html而不是source code
     html = driver.page_source
     driver.quit()
@@ -103,18 +34,18 @@ if __name__ =='__main__':
         except KeyError as err:
             log = 'youtube_download_log.txt'
             with open(log ,'a+' ,encoding='utf-8') as fp:
-                fp.write(tag + '\r\n' + err)
+                fp.write('tag:\r\n' + tag + 'error message:\r\n' + err + '='*30 + '\r\n')
     # print(download_urls)
     
     #將download_urls每3個分為一組
-    download_lists = split_list(download_urls ,2)
+    download_lists = my.split_list(download_urls ,2)
     
     #創建process pool
     #本來使用8個process現在改用2個process
     pool =p.Pool(processes=2)
     for download_list in download_lists:
         print('將' ,download_list ,'放入pool中')
-        pool.apply_async(process_create_thread_to_downlad , args = (download_list,))
+        pool.apply_async(my.process_create_thread_to_downlad , args = (download_list,))
         time.sleep(5)
     #關閉pool，防止新的工作進入pool，同時等待子進程完成
     pool.close()
